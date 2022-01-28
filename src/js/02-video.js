41158a995e1a88641373729029d2edb8156e2941
Serhii Plis <serzh2577@gmail.com>
2022-01-28 17:09:36 +0200

    fix bug task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,22 @@
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-
-const STORAGE_KEY = 'videoplayer-current-time';
 const player = new Vimeo.Player(iframe);
 
 player.on('timeupdate', throttle(onTimeupdate, 1000));
 
+const STORAGE_KEY = 'videoplayer-current-time';
+
 function onTimeupdate(time) {
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(time));
+  localStorage.setItem(STORAGE_KEY, time.seconds);
 }
 
-player.setCurrentTime(`${JSON.parse(localStorage.getItem(STORAGE_KEY)).seconds}`);
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY)).catch(function (error) {
+  switch (error.name) {
+    case 'RangeError':
+      break;
+
+    default:
+      break;
+  }
+});
